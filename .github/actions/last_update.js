f8d679c77fe04f617b932eae328f01bba9df288a
Milan Somsedík <milan.somsedik@gmail.com>
2020-10-06 14:02:45 +0200

    ccccc

--- .github/actions/last_update.js
+++ .github/actions/last_update.js
@@ -1,14 +1,15 @@
 var fs = require('fs');
-var JSZip = require('./jszip');
+//var JSZip = require('./jszip');
 var date = new Date();
+var archiver = require('archiver');
 
-var zip = new JSZip()
+/*var zip = new JSZip()
 var data = fs.readFile('main/update.json')
 zip.file('updatein.json', data, {base64: true})
 
 
 zip.generateNodeStream({type:'nodebuffer',streamFiles:true})
-   .pipe(fs.createWriteStream('main/update.zip'))  
+   .pipe(fs.createWriteStream('main/update.zip'))  */
 
 
 fs.writeFile('main/update.json', '{"update":"' + date.toISOString() + '"}', function (err) {
