0cc43b6a20e99b81d50bb0296b3eec53fc971831
Milan Somsedík <milan.somsedik@gmail.com>
2020-10-06 12:04:04 +0200

    c 9

--- .github/actions/last_update.js
+++ .github/actions/last_update.js
@@ -1,5 +1,11 @@
 var fs = require('fs');
+var archiver = require('archiver');
 var date = new Date();
+
+var zip = archiver('zip');
+zip.pipe('main/update.zip')
+zip.append('main/update.json')
+zip.finalize();
 
 fs.writeFile('main/update.json', '{"update":"' + date.toISOString() + '"}', function (err) {
   if (err) throw err;
