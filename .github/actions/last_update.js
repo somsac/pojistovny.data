5e374a0934ec25a04a5737109e42936bc28a2a69
Milan Somsedík <milan.somsedik@gmail.com>
2020-10-15 15:09:49 +0200

    uprava 1

--- .github/actions/last_update.js
+++ .github/actions/last_update.js
@@ -2,22 +2,31 @@ var fs = require('fs');
 var date = new Date();
 const archiver = require('archiver');
 
-var output = fs.createWriteStream('main/example.zip');
-var archive = archiver('zip', {
-  zlib: { level: 9 } 
-});
-archive.pipe(output);
-archive.file('main/logo/ace.png', { name: 'ace.png' });
+
+
+
 archive.file('main/logo/aegon.png', { name: 'aegon.png' });
-archive.finalize();
+
 
 fs.readFile('main/data.json', (err, data) => {
   let pojistovny = JSON.parse(data);
-  for (index in pojistovny.data) {
-    var pojistovna = pojistovny.data[index];
-    console.log(pojistovna.logo);
+  let zip_name_all = pojistovny.logos_zip_file_all;
+  let zip_name_automobil = pojistovny.logos_zip_file_automobil;
+
+  var outputAll = fs.createWriteStream('main/' + zip_name_all);
+  var archiveAll = archiver('zip', {
+    zlib: { level: 9 } 
+  });
+  archiveAll.pipe(outputAll);
+
+  for (index in pojistovny.insurers) {
+    var pojistovna = pojistovny.insurers[index];
+    archive.file('main/logo/' + pojistovna.logo, { name: pojistovna.logo });
+  //  console.log(pojistovna.logo);
   }
 });
+
+archiveAll.finalize();
 
 
 fs.writeFile('main/update.json', '{"update":"' + date.toISOString() + '"}', function (err) {
