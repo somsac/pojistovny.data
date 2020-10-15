81341482125e837beb11d264f9bda246038dbadf
Milan Somsedík <milan.somsedik@gmail.com>
2020-10-15 15:36:27 +0200

    Update last_update.js

--- .github/actions/last_update.js
+++ .github/actions/last_update.js
@@ -14,17 +14,33 @@ fs.readFile('main/data.json', (err, data) => {
   });
   archiveAll.pipe(outputAll);
 
+  var outputAuto = fs.createWriteStream('main/' + zip_name_automobil);
+  var archiveAauto = archiver('zip', {
+    zlib: { level: 9 } 
+  });
+  archiveAuto.pipe(outputAuto);
+
   var logos = [];
+  var logosAuto = [];
   for (index in pojistovny.insurers) {
     var pojistovna = pojistovny.insurers[index];
     if (logos[pojistovna.logo] != 1) {
       archiveAll.file('main/logo/' + pojistovna.logo, { name: pojistovna.logo });
       logos[pojistovna.logo] = 1;
     }
+
+    if (pojistovna.app_automobil.use_in_app) {
+      if (logosAuto[pojistovna.logo] != 1) {
+        archiveAuto.file('main/logo/' + pojistovna.logo, { name: pojistovna.logo });
+        logosAuto[pojistovna.logo] = 1;
+      }
+    }
+
   //  console.log(pojistovna.logo);
   }
 
   archiveAll.finalize();
+  archiveAuto.finalize();
 });
 
 
